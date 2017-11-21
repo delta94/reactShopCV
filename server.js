f511ce587c204f51100cfa14f58d4caae3a70d22
Tiago Rodrigues <tmcrs123@gmail.com>
2017-11-21 21:58:53 +0000

    deploy test

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname,'src')));
-console.log(path.join(__dirname,'src'));
+app.use(express.static(__dirname));
+// console.log(path.join(__dirname,'src'));
 
 app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname,'index.html'))
